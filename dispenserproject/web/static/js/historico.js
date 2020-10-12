25a8ab1a656efda42ff7794af22dd217f4b7603e
Daniel Barbosa <daniel_herbert_barbosa@hotmail.com>
2020-10-11 23:57:39 -0300

    mobile historico

--- dispenserproject/web/static/js/historico.js
+++ dispenserproject/web/static/js/historico.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
         paging: true,
         searching: true,
         ordering: true,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese.json"
+        },
+        rowReorder: {
+            selector: 'td:nth-child(4)'
+        },
+        responsive: true,
         "columnDefs": [
             {
                 "targets": [ 5 ],
